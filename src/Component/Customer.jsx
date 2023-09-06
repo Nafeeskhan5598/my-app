b94fcc69453371833fd446f4865d9b33e171861d
nafees khan <nafees@codernaline.com>
2023-09-06 06:34:27 +0530

    complete website in react js

--- src/Component/Customer.jsx
+++ src/Component/Customer.jsx
@@ -4,10 +4,8 @@ import ChildCom from "./ChildCom";
 import Firstimg from '../images/Ellipse 6 (1).svg';
 import Secondimg from '../images/Ellipse 6 (2).svg';
 import Thirdimg from '../images/Ellipse 6.svg';
-
-
 function Customer() {
-
+                                                                    // customer component
     return (
         <>
             <div className="container customer-text-area">
@@ -15,20 +13,20 @@ function Customer() {
                     <h6>Customer Comments</h6>
                     <h3>Why customers love us</h3>
                     <h2>They are doing amazing job
-                         with hundred percent customer satisfaction, Love their work 
-                         and would like to work with them again</h2>
+                        with hundred percent customer satisfaction, Love their work
+                        and would like to work with them again</h2>
                 </div>
                 <div className="person-all-area">
-                <div className="person-all border-boxy wow animate__animated animate__fadeInUp">
-                   <ChildCom imgsrc= { Thirdimg }  header="Pierre Hackett" pera="VP of Engineering" />
-                  </div>
-                  <div className="person-all wow animate__animated animate__fadeInUp">
-                   <ChildCom imgsrc= {  Firstimg }  header="Natalia Sanz" pera="Head of Technology" />
-                  </div>
-                  <div className="person-all wow animate__animated animate__fadeInUp">
-                   <ChildCom imgsrc= {  Secondimg }  header="Ece Akman" pera="Senior Marketer" />
-                  </div>
-                  </div>
+                    <div className="person-all border-boxy wow animate__animated animate__fadeInUp">
+                        <ChildCom imgsrc={Thirdimg} header="Pierre Hackett" pera="VP of Engineering" />
+                    </div>
+                    <div className="person-all wow animate__animated animate__fadeInUp">
+                        <ChildCom imgsrc={Firstimg} header="Natalia Sanz" pera="Head of Technology" />
+                    </div>
+                    <div className="person-all wow animate__animated animate__fadeInUp">
+                        <ChildCom imgsrc={Secondimg} header="Ece Akman" pera="Senior Marketer" />
+                    </div>
+                </div>
 
             </div>
 
